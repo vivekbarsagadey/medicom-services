2cb728df347ff714692df3317695c57fc8d113a1
vivek <vivek.bnb@gmail.com>
2018-09-25 16:00:46 +0530

    patient data stored on database

--- src/com/medicom/health/diabetes/parser/patient_data_parser.py
+++ src/com/medicom/health/diabetes/parser/patient_data_parser.py
@@ -1,10 +1,21 @@
 
 from com.medicom.health.diabetes.domain.patient_features import PatientFeatures
+import csv
+import json
 
-class PatientDataParser:
+csvfile = open('C:/Users/user/Desktop/user_diabetes.csv', 'r')
+jsonfile = open('C:/Users/user/Desktop/user_data.json', 'w')
+
+class PatientDataParser():
 
     def getPatientListFromFile(self, user=None, fileName=None):
         print("user",user)
         print("fileName", fileName)
         patientFeaturesList = []
         patientFeaturesList.append(PatientFeatures());
+        reader = csv.DictReader(csvfile, patientFeaturesList)
+        for row in reader:
+            json.dump(row, jsonfile)
+            jsonfile.write('\n')
+
+        return True
