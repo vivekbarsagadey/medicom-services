d6e2d01e0996eff2e868aee776fb17e4443a74ab
vivek <vivek.bnb@gmail.com>
2018-09-28 21:36:30 +0530

    miner issue with path description sloved

--- src/com/medicom/health/diabetes/services/store_handler.py
+++ src/com/medicom/health/diabetes/services/store_handler.py
@@ -1,19 +1,16 @@
 import  os
-
-BASE_FLODER = os.path.abspath(os.path.dirname(__name__))
-NEXT_PATH="/com/medicom/health/diabetes/store/"
-FULL_PATH = BASE_FLODER + NEXT_PATH
-
-
-
 import pickle
 import sys
 from pathlib import Path
 
+BASE_FLODER = os.path.abspath(os.path.dirname(__name__))
+NEXT_PATH="/com/medicom/health/diabetes/store/"
+FULL_PATH = BASE_FLODER + NEXT_PATH
+file_ext = '.sav'
 
 class StoreHandle:
     def getModel(self, name="all_model"):
-        config = Path(path+name+file_ext)
+        config = Path(FULL_PATH+name+file_ext)
         return pickle.load(open(FULL_PATH+name+file_ext, 'rb'))
 
     def saveModel(self , name="all_model", model=None):
