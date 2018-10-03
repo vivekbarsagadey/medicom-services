965f494d50bf5a9fe48cade65143b947d781cb3a
vivek <vivek.bnb@gmail.com>
2018-10-03 19:04:38 +0530

    auto file removal after database upload

--- src/com/medicom/health/diabetes/parser/patient_data_parser.py
+++ src/com/medicom/health/diabetes/parser/patient_data_parser.py
@@ -1,18 +1,20 @@
 import csv
 import re
 import os
+import glob
 from com.medicom.health.diabetes.parser.patient_features import PatientFeatures
 
 UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__name__))
 UPLOAD_FOLDER_PATH="/com/medicom/health/diabetes/data/upload/"
 Final_Path = UPLOAD_FOLDER+UPLOAD_FOLDER_PATH
-csvfile = open(os.path.join(Final_Path,'user_diabetes.csv'), 'r')
+extension = 'csv'
 fieldname = ('patientId','pregnancy','glucose','bloodpressure','skinThickness','insulin','bmi','diabetesPedigreeFunction','age')
-word_regex_pattern = re.compile("[^A-Za-z]+")
 
+word_regex_pattern = re.compile("[^A-Za-z]+")
 def camel(chars):
   words = word_regex_pattern.split(chars)
   return "".join(w.lower() if i is 0 else w.title() for i, w in enumerate(words))
+
 
 class PatientDataParser():
     def __init__(self):
@@ -20,13 +22,19 @@ class PatientDataParser():
 
     def getPatientListFromFile(self):
         print("patient features loaded")
-        reader = csv.DictReader(csvfile,fieldname)
-        for row in reader:
-            #print("new row",row)
-            print("Storing the Features in Database")
-            patientFeatures = PatientFeatures(org=row)
-            #print("age >>> ",patientFeatures.age)
-            patientFeatures.save();
-            print("Features saved to database sucessfully.")
-
+        os.chdir(Final_Path)
+        file = [i for i in glob.glob('*.{}'.format(extension))]
+        for csvfile in file:
+            name = open(os.path.join(Final_Path, csvfile), 'r')
+            reader = csv.DictReader(name,fieldname)
+            next(reader)
+            for row in reader:
+                #print("new row",row)
+                print("Storing the Features in Database")
+                patientFeatures = PatientFeatures(org=row)
+                #print("age >>> ",patientFeatures.age)
+                patientFeatures.save();
+                print("Features saved to database sucessfully.")
+            csvfile.close()
+            print("The locally stored file is deleted")
         return True
